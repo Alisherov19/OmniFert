00248084e6ea29594c1f4d248bdb77822514aaa2
Alisherov19 <dhrhhffjf@gmail.com>
2025-05-24 14:58:56 +0300

    formfsdkf

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -4,7 +4,7 @@ module.exports = (async()=>{
     try{
       await sequelize.authenticate()
       console.log("DB connected successfully");
-      await sequelize.sync({force:false})
+      await sequelize.sync({force:true})
       //  await sequelize.sync({alter:true})
       console.log("DB synced successfully ");
     }
