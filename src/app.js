5bfe60dbb96a85e4366efad3e0ee8015c89da953
Alisherov19 <dhrhhffjf@gmail.com>
2025-05-24 20:42:45 +0300

    first commitfsd

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const app = express()
+const cors = require("cors")
 const errorHandler = require("./middlewares/errorHandler")
 //imports
 const router = require("./routes")
 //imports
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 //Routes
