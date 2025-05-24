aa01ea8d21c6a08daebd21d5413ed3859fe2cf66
Alisherov19 <dhrhhffjf@gmail.com>
2025-05-24 21:16:40 +0300

    first

--- src/config/sequelize.config.js
+++ src/config/sequelize.config.js
@@ -1,12 +1,16 @@
-const {Sequelize} = require("sequelize")
+const { Sequelize } = require("sequelize");
+
 require("dotenv").config()
 
+const sequelize = new Sequelize(process.env.DB_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // Render uchun kerak
+    }
+  },
+  logging:false
+});
 
-const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD,{
-    host:process.env.DB_HOST,
-    port:process.env.DB_PORT,
-    dialect:"postgres",
-    logging:false
-})
-
-module.exports = sequelize
+module.exports = sequelize;
